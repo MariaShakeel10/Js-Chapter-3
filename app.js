bd5c5d1375c9f21409e617ec59576a6282097652
MariaShakeel10 <mariashakeel10a@gmail.com>
2024-05-08 17:19:55 +0500

    completed

---
+++ app.js
@@ -0,0 +1,16 @@
+var age = 18;
+alert(`I am ${age} years old.`);
+
+var timesVisit = "1";
+alert(`You have visited this page ${timesVisit} times`);
+
+var birthYear = 2005;
+document.write (`My Birth year is <b>${birthYear}.</b></p>`);
+
+document.write (`<p> The data type of my declared variable is<b> ${typeof(birthYear)}</b></p>`);
+
+var visitorName=prompt("Enter your name");
+var productName=prompt("Enter your product name");
+var quantityOfProducts=prompt("Enter your products quantity");
+
+document.write (`<p><b>${visitorName}</b> ordered <b>${quantityOfProducts}${productName}</b> on XYZ clothing store.</p>`)
